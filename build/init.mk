5ae69c3abd443c4400171a790c8067c531dc71c4
Jaromil <jaromil@dyne.org>
2024-10-28 23:19:24 +0100

    fix: reorganize repl code
    move repl specific code outside main cjit.c

--- build/init.mk
+++ build/init.mk
@@ -24,7 +24,7 @@ cflags := ${CFLAGS} ${cflags_includes}
 
 SOURCES := src/io.o src/file.o src/cflag.o src/cjit.o \
            src/embed-libtcc1.o src/embed-headers.o \
-           src/exec-headers.o
+           src/exec-headers.o src/repl.o
 
 ldadd := lib/tinycc/libtcc.a
 
