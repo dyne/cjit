5a837c85ca28e4227510c4dd5466e7cd70e21e82
Jaromil <jaromil@dyne.org>
2024-11-16 17:01:51 +0100

    feat: new commandline parser
    using ketopt to support duplicate option switches, now triggering also define,
    lib path and multiple files to add.

--- src/cjit.c
+++ src/cjit.c
@@ -34,6 +34,8 @@
 #include <libtcc.h>
 #include <unistd.h>
 
+#include <ketopt.h>
+
 // from file.c
 extern long  file_size(const char *filename);
 extern char* file_load(const char *filename);
@@ -69,38 +71,53 @@ void handle_error(void *n, const char *m) {
 }
 
 int main(int argc, char **argv) {
-  TCCState *TCC;
-  const char *syntax = "[options] code.c";
+  TCCState *TCC = NULL;
   // const char *progname = "cjit";
-  static bool verbose = false;
-  static bool version = false;
   char tmptemplate[] = "/tmp/CJIT-exec.XXXXXX";
   char *tmpdir = NULL;
-  char *code = NULL;
   int res = 1;
 
-  static const struct cflag options[] = {
-    CFLAG(bool, "verbose", 'v', &verbose, "Verbosely show progress"),
-    CFLAG(bool, "version", 'V', &version, "Show build version"),
-    CFLAG_HELP,
-    CFLAG_END
-  };
-  cflag_apply(options, syntax, &argc, &argv);
-  const char *code_path = argv[0];
   _err("CJIT %s by Dyne.org",VERSION);
-  if(version) exit(0); // print version and exit
 
   TCC = tcc_new();
   if (!TCC) {
     _err("Could not initialize tcc");
     exit(1);
   }
+
   // get the extra cflags from the CFLAGS env variable
+  // they are overridden by explicit command-line options
   if(getenv("CFLAGS")) {
     char *extra_cflags = NULL;
     extra_cflags = getenv("CFLAGS");
     _err("CFLAGS: %s",extra_cflags);
     tcc_set_options(TCC, extra_cflags);
+  }
+
+  ketopt_t opt = KETOPT_INIT;
+  int i, c;
+  while ((c = ketopt(&opt, argc, argv, 1, "vD:L:l:C:I:", NULL)) >= 0) {
+    if (c == 'v') {
+      _err("Running version: %s\n",VERSION);
+      tcc_delete(TCC);
+      exit(0); // print version and exit
+    } else if (c == 'D') { // define
+      _err("define: %s",opt.arg);
+      tcc_define_symbol(TCC, opt.arg, NULL); // TODO: sym=var
+    } else if (c == 'L') { // library path
+      _err("lib path: %s",opt.arg);
+      tcc_add_library_path(TCC,tmpdir);
+    } else if (c == 'l') { // library link
+      _err("lib: %s",opt.arg);
+      tcc_add_library(TCC, opt.arg);
+    } else if (c == 'C') { // library link
+      _err("cflags: %s",opt.arg);
+      tcc_set_options(TCC, opt.arg);
+    } else if (c == 'I') { // library link
+      tcc_add_include_path(TCC,opt.arg);
+    }
+    else if (c == '?') _err("unknown opt: -%c\n", opt.opt? opt.opt : ':');
+    else if (c == ':') _err("missing arg: -%c\n", opt.opt? opt.opt : ':');
   }
 
   // initialize the tmpdir for execution
@@ -126,55 +143,57 @@ int main(int argc, char **argv) {
   tcc_add_libc_symbols(TCC);
 #endif
 
-  if (argc == 0) {
-      printf("No input file: running in interactive mode\n");
-#ifdef REPL_SUPPORTED
-      res = cjit_cli_kilo(TCC);
-#else
-      res = cjit_cli_tty(TCC);
-#endif
-      goto endgame;
-  }
-  _err("Source: %s",code_path);
+//   if (argc == 0) {
+//       printf("No input file: running in interactive mode\n");
+// #ifdef REPL_SUPPORTED
+//       res = cjit_cli_kilo(TCC);
+// #else
+//       res = cjit_cli_tty(TCC);
+// #endif
+//       goto endgame;
+//   }
+//   _err("Source: %s",code_path);
 
 
-#ifndef LIBC_MINGW32 // POSIX only
-  struct stat st;
-  if (stat(code_path, &st) == -1) {
-    _err("File not found: %s",code_path);
-    goto endgame;
+  _err("Source code:");
+  for (i = opt.ind; i < argc; ++i) {
+    const char *code_path = argv[i];
+    // TODO: load entire directory
+// #ifndef LIBC_MINGW32 // POSIX only
+//     struct stat st;
+//     if (stat(code_path, &st) == -1) {
+//       _err("File not found: %s",code_path);
+//       goto endgame;
+//     }
+//     if (S_ISDIR(st.st_mode)) {
+//       _err("+ %s (dir)", code_path);
+//       tcc_add_include_path(TCC, code_path);
+//       code = dir_load(code_path);
+//   } else {
+//     code = file_load(code_path);
+//   }
+// #else
+//   /* TODO: Add Windows support for directory */
+//   code = file_load(code_path);
+// #endif
+    _err("+ %s",code_path);
+    // TODO: check if file exists
+    tcc_add_file(TCC, code_path);
   }
-  if (S_ISDIR(st.st_mode)) {
-    _err("%s: it is a directory path. Recursively adding all sources.", code_path);
-    tcc_add_include_path(TCC, code_path);
-    code = dir_load(code_path);
-  } else {
-    code = file_load(code_path);
-  }
-#else
-  /* TODO: Add Windows support for directory */
-  code = file_load(code_path);
-#endif
 
-  char *err_msg = NULL;
-  if(!code) {
-    _err("File not found: %s",code_path);
-    goto endgame;
-  }
-
-#ifdef REPL_SUPPORTED
-  _err("Start execution\n---------------");
-  res = cjit_compile_and_run(TCC, code, argc, argv, 1, &err_msg);
-  if (err_msg) {
-        _err(err_msg);
-  }
-#else
+// #ifdef REPL_SUPPORTED
+//   _err("Start execution\n---------------");
+//   res = cjit_compile_and_run(TCC, code, argc, argv, 1, &err_msg);
+//   if (err_msg) {
+//         _err(err_msg);
+//   }
+// #else
   // error handler callback for TCC
   tcc_set_error_func(TCC, stderr, handle_error);
-  if (tcc_compile_string(TCC, code) == -1) return 1;
-  free(code); // safe: bytecode compiled is in TCC now
-  code = NULL;
-  _err("Compilation successful");
+  // if (tcc_compile_string(TCC, code) == -1) return 1;
+  // free(code); // safe: bytecode compiled is in TCC now
+  // code = NULL;
+  // _err("Compilation successful");
 
   // relocate the code
   if (tcc_relocate(TCC) < 0) {
@@ -187,11 +206,13 @@ int main(int argc, char **argv) {
 #else
   res = cjit_exec_win(TCC, "main", argc, argv);
 #endif
-#endif // REPL_SUPPORTED
+// #endif // REPL_SUPPORTED
+
+  // if( tcc_run(TCC,argc,argv) == -1) res=1; else res=0;
+
   endgame:
   // free TCC
-  free(code);
-  tcc_delete(TCC);
+  if(TCC) tcc_delete(TCC);
   if(tmpdir) {
     rm_recursive(tmpdir);
   }
