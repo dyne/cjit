6cb3c6006de0823a885584de8624d45e11602eea
Jaromil <jaromil@dyne.org>
2024-12-01 06:27:46 +0100

    fix: small optimization to usleep on windows

--- src/win-compat.c
+++ src/win-compat.c
@@ -3,6 +3,6 @@
 // Define the usleep function for Windows
 void win_compat_usleep(unsigned int microseconds) {
     // Convert microseconds to milliseconds
-    unsigned int milliseconds = microseconds / 1000;
+    unsigned int milliseconds = microseconds >> 10; // /1024;
     Sleep(milliseconds);
 }
