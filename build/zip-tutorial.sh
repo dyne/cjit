eeae915bf6cb9bee75c1f11b54ce90d5068cff18
Jaromil <jaromil@dyne.org>
2024-12-12 10:29:52 +0100

    fix: release also an archive with all binaries for the tutorial
    this will pack cjit-tutorial.zip as part of the released files to ease
    the journey on https://dyne.org/docs/cjit

---
+++ build/zip-tutorial.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# assume all binaries are in cjit-bin/release*/*
+# from github action release assets
+
+mkdir -p cjit-tutorial
+cp -ra cjit-bin/release/*  cjit-tutorial
+cp -ra examples            cjit-tutorial
+git clone --depth 0 https://github.com/dyne/docs
+cp -ra docs/src/cjit       cjit-tutorial/docs
+
+zip a cjit-tutorial.zip    cjit-tutorial
