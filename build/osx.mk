72b5a1aca1f0a5f738d30a14350278bf5abc9b8c
Jaromil <jaromil@dyne.org>
2025-01-10 17:50:51 +0100

    fix: protect flags and no codesign for osx compat

--- build/osx.mk
+++ build/osx.mk
@@ -2,8 +2,10 @@ include build/init.mk
 
 cc := clang
 cflags += -DCJIT_BUILD_OSX
+extra_tinycc_config += --config-codesign=no
 
 all: embed-posix cjit.command
+
 
 cjit.command: ${SOURCES}
 	$(cc) $(cflags) -o $@ $(SOURCES) ${ldflags} ${ldadd}
